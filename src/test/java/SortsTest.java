deec36ac4b4108ab24418ad635b64c52a3a590f2
DmitryErmakoff <dimka6466erm@gmail.com>
2024-10-17 15:44:08 +0500

    add validateDigitsInput

--- src/test/java/SortsTest.java
+++ src/test/java/SortsTest.java
@@ -2,6 +2,7 @@ import org.junit.Test;
 import ru.d3m4k.Main;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class SortsTest {
 
@@ -57,6 +58,46 @@ public class SortsTest {
         assertThat(array).containsExactly(1, 2, 3, 4, 5);
         Main.quickSort(array, 0, array.length - 1);
         assertThat(array).containsExactly(1, 2, 3, 4, 5);
+    }
+
+    @Test
+    public void testValidInput() throws Exception {
+        String validInput = "1234567890";
+        Main.validateInput(validInput);
+    }
+
+    @Test
+    public void testInputWithLetters() {
+        String invalidInput = "123abc";
+        assertThatThrownBy(() -> Main.validateInput(invalidInput))
+                .isInstanceOf(Exception.class)
+                .hasMessage("Input string must contain only digits");
+    }
+
+    @Test
+    public void testInputWithSpecialCharacters() {
+        String invalidInput = "123!@#";
+        assertThatThrownBy(() -> Main.validateInput(invalidInput))
+                .isInstanceOf(Exception.class)
+                .hasMessage("Input string must contain only digits");
+    }
+
+    @Test
+    public void testEmptyInput() {
+        String emptyInput = "";
+        try {
+            Main.validateInput(emptyInput);
+        } catch (Exception e) {
+            throw new AssertionError("Method should not throw an exception for empty input", e);
+        }
+    }
+
+    @Test
+    public void testInputWithSpaces() {
+        String invalidInput = "123 456";
+        assertThatThrownBy(() -> Main.validateInput(invalidInput))
+                .isInstanceOf(Exception.class)
+                .hasMessage("Input string must contain only digits");
     }
 
 }
